05941618da4033eefe968076c6413c4aeaa48e5c
huecraft143 <mattiapalano11@gmail.com>
2021-02-08 15:25:15 +0100

    Aggiustati import in main

--- main.py
+++ main.py
@@ -17,8 +17,7 @@
 # Press the green button in the gutter to run the script.
 
 import random as random
-from Classifier import main
-import Map
+from Classifier import classify
 from Map import Node
 from Map import Graph
 from Emergenza import Emergency as Emergenza
