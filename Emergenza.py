120d02c888670cc2fbfc71645bfcd694194eeeba
Enrico Pallotta <pallottenrico@gmail.com>
2021-02-08 11:26:40 +0100

    Generata classe emergenza e aggiornato il README.md

---
+++ Emergenza.py
@@ -0,0 +1,22 @@
+import Graph
+
+class Emergenza:
+
+    tempo : int
+    num_agenti : int
+    num_speciali : int
+    num_veicoli : int
+
+    def __init__(self):
+        self.tempo = 0
+        self.num_agenti = 0
+        self.num_speciali = 0
+        self.num_veicoli = 0
+
+    def __init__(self, tempo, num_agenti, num_speciali, num_veicoli, luogo : Graph.Node):
+        self.tempo = tempo
+        self.num_agenti = num_agenti
+        self.num_speciali = num_speciali
+        self.num_veicoli = num_veicoli
+        self.luogo = luogo
+
